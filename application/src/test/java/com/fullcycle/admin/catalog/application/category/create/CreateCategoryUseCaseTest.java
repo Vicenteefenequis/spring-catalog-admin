7e50fb3694cba8db62b1e2c49cdce8fd4c30018d
Vicente Junior <vicente19981@live.com>
2023-04-07 14:20:53 -0300

    test: ensure CreateCategoryUseCase throws random exception when call Gateway

--- application/src/test/java/com/fullcycle/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
@@ -76,11 +76,11 @@ public class CreateCategoryUseCaseTest {
                 expectedIsActive
         );
 
-        final var actualException = Assertions.assertThrows(DomainException.class,() -> useCase.execute(aCommand));
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
 
-        Assertions.assertEquals(expectedErrorMessage,actualException.getMessage());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
 
-        Mockito.verify(categoryGateway,times(0)).create(any());
+        Mockito.verify(categoryGateway, times(0)).create(any());
     }
 
     @Test
@@ -101,6 +101,37 @@ public class CreateCategoryUseCaseTest {
 
         Assertions.assertNotNull(actualOutput);
         Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, times(1)).create(argThat(aCategory ->
+                Objects.equals(expectedName, aCategory.getName()) &&
+                        Objects.equals(expectedDescription, aCategory.getDescription()) &&
+                        Objects.equals(expectedIsActive, aCategory.isActive()) &&
+                        Objects.nonNull(aCategory.getId()) &&
+                        Objects.nonNull(aCategory.getCreatedAt()) &&
+                        Objects.nonNull(aCategory.getUpdatedAt()) &&
+                        Objects.nonNull(aCategory.getDeletedAt())
+        ));
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAException() {
+
+        final var expectedName = "filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+
+        when(categoryGateway.create(any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException = Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(categoryGateway, times(1)).create(any());
 
         Mockito.verify(categoryGateway, times(1)).create(argThat(aCategory ->
                 Objects.equals(expectedName, aCategory.getName()) &&
