88aa0a5913b95ac56c192de9c364d9465b4264c0
Vicente Junior <vicente19981@live.com>
2023-04-07 17:16:50 -0300

    feat: create get category

---
+++ application/src/main/java/com/fullcycle/admin/catalog/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
@@ -0,0 +1,31 @@
+package com.fullcycle.admin.catalog.application.category.retrieve.get;
+
+import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalog.domain.category.CategoryID;
+import com.fullcycle.admin.catalog.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalog.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultGetCategoryByIdUseCase(CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public CategoryOutput execute(String anIn) {
+        final var anCategoryId = CategoryID.from(anIn);
+        return this.categoryGateway.findById(anCategoryId)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(anCategoryId));
+    }
+
+    private static Supplier<DomainException> notFound(CategoryID anId) {
+        return () -> DomainException.with(new Error("Category with ID %s was  not found".formatted(anId.getValue())));
+    }
+
+}
