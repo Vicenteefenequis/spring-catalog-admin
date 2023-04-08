a23c8c0614022a1b3ad92a819e3d615bcff15425
Vicente Junior <vicente19981@live.com>
2023-04-08 14:36:15 -0300

    feat: create jpa category repository interface

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/MainTest.java
@@ -1,8 +1,14 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
+import com.fullcycle.admin.catalog.domain.category.Category;
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 public class MainTest {
 
@@ -12,4 +18,6 @@ public class MainTest {
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
+
+
 }
