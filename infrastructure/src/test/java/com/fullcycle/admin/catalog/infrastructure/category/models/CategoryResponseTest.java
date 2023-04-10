7a1a6fc3edc4c7b2eaa65b1641e278c3b625b8c7
Vicente Junior <vicente19981@live.com>
2023-04-10 20:52:09 -0300

    test: unmarshall json parse category response

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/models/CategoryResponseTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/models/CategoryResponseTest.java
@@ -15,7 +15,7 @@ public class CategoryResponseTest {
     private JacksonTester<CategoryResponse> json;
 
     @Test
-    public void testMarshall() throws  Exception{
+    public void testMarshall() throws Exception {
         final var expectedId = "123";
         final var expectedName = "Filmes";
         final var expectedDescription = "A melhor categoria de filmes";
@@ -37,12 +37,55 @@ public class CategoryResponseTest {
 
         final var actualJson = this.json.write(response);
         Assertions.assertThat(actualJson)
-                .hasJsonPathValue("$.id",expectedId)
-                .hasJsonPathValue("$.name",expectedName)
-                .hasJsonPathValue("$.description",expectedDescription)
-                .hasJsonPathValue("$.is_active",expectedActive)
-                .hasJsonPathValue("$.created_at",expectedCreatedAt.toString())
-                .hasJsonPathValue("$.updated_at",expectedUpdatedAt.toString())
-                .hasJsonPathValue("$.deleted_at",expectedDeletedAt.toString());
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.is_active", expectedActive)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt.toString())
+                .hasJsonPathValue("$.updated_at", expectedUpdatedAt.toString())
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt.toString());
+    }
+
+    @Test
+    public void testUnmarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A melhor categoria de filmes";
+        final var expectedActive = true;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+
+        final var json = """
+                {
+                    "id": "%s",
+                    "name": "%s",
+                    "description": "%s",
+                    "is_active": %s,
+                    "created_at": "%s",
+                    "updated_at": "%s",
+                    "deleted_at": "%s"
+                }
+                """.formatted(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedActive,
+                expectedCreatedAt.toString(),
+                expectedUpdatedAt.toString(),
+                expectedDeletedAt.toString()
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("id", expectedId)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedActive)
+                .hasFieldOrPropertyWithValue("createdAt", expectedCreatedAt)
+                .hasFieldOrPropertyWithValue("updatedAt", expectedUpdatedAt)
+                .hasFieldOrPropertyWithValue("deletedAt", expectedDeletedAt);
     }
 }
