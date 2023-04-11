4bdbbe0519b4818a0be0998fab1fd47b62dc263c
Vicente Junior <vicente19981@live.com>
2023-04-11 19:56:59 -0300

    feat: create new genre entity

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/genre/GenreID.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/genre/GenreID.java
@@ -1,28 +1,28 @@
-package com.fullcycle.admin.catalog.domain.category;
+package com.fullcycle.admin.catalog.domain.genre;
 
 import com.fullcycle.admin.catalog.domain.Identifier;
 
 import java.util.Objects;
 import java.util.UUID;
 
-public class CategoryID extends Identifier {
+public class GenreID extends Identifier {
     private final String value;
 
-    public CategoryID(String value) {
+    public GenreID(String value) {
         Objects.requireNonNull(value);
         this.value = value;
     }
 
-    public static CategoryID unique() {
-        return CategoryID.from(UUID.randomUUID());
+    public static GenreID unique() {
+        return GenreID.from(UUID.randomUUID());
     }
 
-    public static CategoryID from(final String anId) {
-        return new CategoryID(anId);
+    public static GenreID from(final String anId) {
+        return new GenreID(anId);
     }
 
-    public static CategoryID from(final UUID anId) {
-        return new CategoryID(anId.toString().toLowerCase());
+    public static GenreID from(final UUID anId) {
+        return new GenreID(anId.toString().toLowerCase());
     }
 
     @Override
@@ -34,7 +34,7 @@ public class CategoryID extends Identifier {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        final CategoryID that = (CategoryID) o;
+        final GenreID that = (GenreID) o;
         return Objects.equals(getValue(), that.getValue());
     }
 
---
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/genre/GenreTest.java
@@ -0,0 +1,101 @@
+package com.fullcycle.admin.catalog.domain.genre;
+
+
+import com.fullcycle.admin.catalog.domain.exceptions.DomainException;
+import com.fullcycle.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenreTest {
+
+
+    @Test
+    public void givenValidParams_whenCallNewGenre_shouldInstantiateGenre() {
+        // given
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+
+        // when
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        // then
+        Assertions.assertNotNull(actualGenre);
+        Assertions.assertNotNull(actualGenre.getId());
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size());
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNull_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        // given
+        final String expectedName = null;
+        final var expectedIsActive = false;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        // when
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        // then
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+
+    @Test
+    public void givenInvalidEmptyName_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        // given
+        final var expectedName = " ";
+        final var expectedIsActive = false;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        // when
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        // then
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallNewGenreAndValidate_shouldReceiveAError() {
+        // given
+        final var expectedName = "" +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, " +
+                "nisl eget ultricies lacinia, nisl nisl lacinia nisl, eget ultricies " +
+                "nisl nisl eget nisl. Donec auctor, nisl eget ultricies lacinia, nisl" +
+                "nisl lacinia nisl, eget ultricies nisl nisl eget nisl. Donec auctor";
+        final var expectedIsActive = false;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
+
+        // when
+        final var actualGenre = Genre.newGenre(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        // then
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+}
