1b6b3abb3b82f73bc5712cceb6cf99fdef23fdc4
Vicente Junior <vicente19981@live.com>
2023-04-18 17:36:45 -0300

    test: ensure UpdateGenreUseCase update throws error with invalid null name

--- application/src/test/java/com/fullcycle/admin/catalog/application/genre/update/UpdateGenreUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalog/application/genre/update/UpdateGenreUseCaseTest.java
@@ -2,6 +2,7 @@ package com.fullcycle.admin.catalog.application.genre.update;
 
 import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
 import com.fullcycle.admin.catalog.domain.category.CategoryID;
+import com.fullcycle.admin.catalog.domain.exceptions.NotificationException;
 import com.fullcycle.admin.catalog.domain.genre.Genre;
 import com.fullcycle.admin.catalog.domain.genre.GenreGateway;
 import org.junit.jupiter.api.Assertions;
@@ -116,6 +117,37 @@ public class UpdateGenreUseCaseTest {
                         && Objects.isNull(aUpdateGenre.getDeletedAt())
 
         ));
+    }
+
+
+    @Test
+    public void givenAnInvalidName_whenCallsUpdateGenre_shouldReturnNotificationException() {
+        //given
+        final var aGenre = Genre.newGenre("Acao", true);
+        final var expectedId = aGenre.getId();
+
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+
+        final var aCommand = UpdateGenreCommand.with(expectedId.getValue(), expectedName, expectedIsActive, asString(expectedCategories));
+
+        when(genreGateway.findById(any()))
+                .thenReturn(Optional.of(Genre.with(aGenre)));
+
+
+        //when
+        final var actualOutput = Assertions.assertThrows(NotificationException.class, () -> useCase.execute(aCommand));
+        //then
+
+        Assertions.assertEquals(expectedErrorCount, actualOutput.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualOutput.getErrors().get(0).message());
+
+        verify(genreGateway, times(1)).findById(eq(expectedId));
     }
 
 
