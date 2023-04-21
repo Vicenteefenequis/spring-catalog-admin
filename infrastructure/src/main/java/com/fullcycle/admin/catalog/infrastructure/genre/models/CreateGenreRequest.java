14170c181fed867f79abcbfaa2afe45b85f95430
Vicente Junior <vicente19981@live.com>
2023-04-21 17:41:48 -0300

    feat: implement create genre controller

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/genre/models/CreateGenreRequest.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/genre/models/CreateGenreRequest.java
@@ -2,12 +2,20 @@ package com.fullcycle.admin.catalog.infrastructure.genre.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.List;
 
 public record CreateGenreRequest(
-        @JsonProperty("id") String id,
         @JsonProperty("name") String name,
         @JsonProperty("categories_ids") List<String> categories,
         @JsonProperty("is_active") Boolean active
 ) {
+
+    public Boolean isActive() {
+        return active != null ? active : true;
+    }
+
+    public List<String> categories() {
+        return this.categories != null ? this.categories : Collections.emptyList();
+    }
 }
