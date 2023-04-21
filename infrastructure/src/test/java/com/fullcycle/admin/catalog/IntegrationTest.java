ce6847933c70efbc9fae6b3878f84e75251d350d
Vicente Junior <vicente19981@live.com>
2023-04-21 15:42:47 -0300

    test: ensure GetGenreByIdUseCase GET a genre integration test

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/IntegrationTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/IntegrationTest.java
@@ -3,6 +3,7 @@ package com.fullcycle.admin.catalog;
 import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -11,6 +12,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test-integration")
+@ComponentScan(basePackages = "com.fullcycle.admin.catalog")
 @SpringBootTest(classes = WebServerConfig.class)
 @ExtendWith(MySQLCleanUpExtension.class)
 public @interface IntegrationTest {
