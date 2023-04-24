e0992c109dd3af3ce8c1a351cac614f310e49ebc
Vicente Junior <vicente19981@live.com>
2023-04-24 13:07:09 -0300

    test: ensure GetCastMemberById get a it identifier

---
+++ application/src/test/java/com/fullcycle/admin/catalog/application/retrieve/get/GetCastMemberByIdUseCaseTest.java
@@ -0,0 +1,80 @@
+package com.fullcycle.admin.catalog.application.retrieve.get;
+
+import com.fullcycle.admin.catalog.application.Fixture;
+import com.fullcycle.admin.catalog.application.UseCaseTest;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.get.DefaultGetCastMemberByIdUseCase;
+import com.fullcycle.admin.catalog.domain.castmember.CastMember;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberGateway;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalog.domain.exceptions.NotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class GetCastMemberByIdUseCaseTest extends UseCaseTest {
+
+
+    @InjectMocks
+    private DefaultGetCastMemberByIdUseCase useCase;
+
+    @Mock
+    private CastMemberGateway castMemberGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(castMemberGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetCastMemberById_shouldReturnIt() {
+        //given
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var aMember = CastMember.newMember(expectedName, expectedType);
+
+        final var expectedId = aMember.getId();
+
+        when(castMemberGateway.findById(any())).thenReturn(Optional.of(aMember));
+        //when
+
+        final var actualOutput = useCase.execute(expectedId.getValue());
+
+        //then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertEquals(expectedId.getValue(), actualOutput.id());
+        Assertions.assertEquals(expectedName, actualOutput.name());
+        Assertions.assertEquals(expectedType, actualOutput.type());
+        Assertions.assertEquals(aMember.getCreatedAt(), actualOutput.createdAt());
+        Assertions.assertEquals(aMember.getUpdatedAt(), actualOutput.updatedAt());
+
+        verify(castMemberGateway).findById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsGetCastMemberAndDoesNotExists_shouldReturnNotFoundException() {
+        //given
+        final var expectedId = CastMemberID.from("invalid-id");
+        final var expectedErrorMessage = "CastMember with ID invalid-id was not found";
+
+
+        when(castMemberGateway.findById(any())).thenReturn(Optional.empty());
+        //when
+
+        final var actualException = Assertions.assertThrows(NotFoundException.class, () -> useCase.execute(expectedId.getValue()));
+
+        //then
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        verify(castMemberGateway, times(1)).findById(eq(expectedId));
+    }
+}
