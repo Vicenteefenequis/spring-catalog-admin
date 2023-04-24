344b93e09edcbf685534182b09f3ffdb4b027659
Vicente Junior <vicente19981@live.com>
2023-04-24 13:07:12 -0300

    feat: ensure GetCastMemberById get a it identifier

---
+++ application/src/main/java/com/fullcycle/admin/catalog/application/castmember/retrieve/get/DefaultGetCastMemberByIdUseCase.java
@@ -0,0 +1,25 @@
+package com.fullcycle.admin.catalog.application.castmember.retrieve.get;
+
+import com.fullcycle.admin.catalog.domain.castmember.CastMember;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberGateway;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalog.domain.exceptions.NotFoundException;
+
+import java.util.Objects;
+
+public final class DefaultGetCastMemberByIdUseCase extends GetCastMemberByIdUseCase {
+
+    private final CastMemberGateway castMemberGateway;
+
+    public DefaultGetCastMemberByIdUseCase(final CastMemberGateway castMemberGateway) {
+        this.castMemberGateway = Objects.requireNonNull(castMemberGateway);
+    }
+
+    @Override
+    public CastMemberOutput execute(String anIn) {
+        final var aMemberId = CastMemberID.from(anIn);
+        return this.castMemberGateway.findById(aMemberId)
+                .map(CastMemberOutput::from)
+                .orElseThrow(() -> NotFoundException.with(CastMember.class, aMemberId));
+    }
+}
