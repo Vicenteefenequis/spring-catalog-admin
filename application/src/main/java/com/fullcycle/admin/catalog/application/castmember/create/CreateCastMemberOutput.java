a087432eb8035818bfaa208474c4a18c4a0b3339
Vicente Junior <vicente19981@live.com>
2023-04-26 23:02:35 -0300

    feat: adding new method of cast member id retunr

--- application/src/main/java/com/fullcycle/admin/catalog/application/castmember/create/CreateCastMemberOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalog/application/castmember/create/CreateCastMemberOutput.java
@@ -1,11 +1,16 @@
 package com.fullcycle.admin.catalog.application.castmember.create;
 
 import com.fullcycle.admin.catalog.domain.castmember.CastMember;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberID;
 
 public record CreateCastMemberOutput(
         String id
 ) {
     public static CreateCastMemberOutput from(final CastMember aMember) {
         return new CreateCastMemberOutput(aMember.getId().getValue());
+    }
+
+    public static CreateCastMemberOutput from(final CastMemberID anId) {
+        return new CreateCastMemberOutput(anId.getValue());
     }
 }
