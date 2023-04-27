1f79d5a2e32a5652c216f85dc0c0fbad80d04ea7
Vicente Junior <vicente19981@live.com>
2023-04-26 23:03:34 -0300

    test: ensure CreateCastMemberController create a member

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/api/CastMemberAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/api/CastMemberAPITest.java
@@ -2,14 +2,33 @@ package com.fullcycle.admin.catalog.infrastructure.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalog.ControllerTest;
-import com.fullcycle.admin.catalog.application.castmember.create.CreateCastMemberUseCase;
-import com.fullcycle.admin.catalog.application.castmember.delete.DeleteCastMemberUseCase;
-import com.fullcycle.admin.catalog.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
-import com.fullcycle.admin.catalog.application.castmember.retrieve.list.ListCastMembersUseCase;
-import com.fullcycle.admin.catalog.application.castmember.update.UpdateCastMemberUseCase;
+import com.fullcycle.admin.catalog.Fixture;
+import com.fullcycle.admin.catalog.application.castmember.create.CreateCastMemberOutput;
+import com.fullcycle.admin.catalog.application.castmember.create.DefaultCreateCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.delete.DefaultDeleteCastMemberUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.get.DefaultGetCastMemberByIdUseCase;
+import com.fullcycle.admin.catalog.application.castmember.retrieve.list.DefaultListCastMembersUseCase;
+import com.fullcycle.admin.catalog.application.castmember.update.DefaultUpdateCastMemberUseCase;
+import com.fullcycle.admin.catalog.domain.castmember.CastMemberID;
+import com.fullcycle.admin.catalog.domain.exceptions.NotificationException;
+import com.fullcycle.admin.catalog.domain.validation.Error;
+import com.fullcycle.admin.catalog.infrastructure.castmember.models.CreateCastMemberRequest;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Objects;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ControllerTest(controllers = CastMemberAPI.class)
 public class CastMemberAPITest {
@@ -20,17 +39,76 @@ public class CastMemberAPITest {
     private ObjectMapper mapper;
 
     @MockBean
-    private CreateCastMemberUseCase createCastMemberUseCase;
+    private DefaultCreateCastMemberUseCase createCastMemberUseCase;
 
     @MockBean
-    private DeleteCastMemberUseCase deleteCastMemberUseCase;
+    private DefaultDeleteCastMemberUseCase deleteCastMemberUseCase;
 
     @MockBean
-    private GetCastMemberByIdUseCase getCastMemberByIdUseCase;
+    private DefaultGetCastMemberByIdUseCase getCastMemberByIdUseCase;
 
     @MockBean
-    private ListCastMembersUseCase listCastMembersUseCase;
+    private DefaultListCastMembersUseCase listCastMembersUseCase;
 
     @MockBean
-    private UpdateCastMemberUseCase updateCastMemberUseCase;
+    private DefaultUpdateCastMemberUseCase updateCastMemberUseCase;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCastMember_shouldReturnItsIdentifier() throws Exception {
+        //given
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+        final var expectedId = CastMemberID.from("any_id");
+
+        final var aCommand = new CreateCastMemberRequest(expectedName, expectedType);
+
+        when(createCastMemberUseCase.execute(any())).thenReturn(CreateCastMemberOutput.from(expectedId));
+
+        //when
+        final var aRequest = post("/cast_members").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(aCommand));
+
+
+        final var response = this.mvc.perform(aRequest).andDo(print());
+
+        //then
+
+        response.andExpect(status().isCreated()).andExpect(header().string("Location", "/cast_members/" + expectedId.getValue())).andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE)).andExpect(jsonPath("$.id", equalTo(expectedId.getValue())));
+
+        verify(createCastMemberUseCase).execute(argThat(actualCmd ->
+                Objects.equals(expectedName, actualCmd.name()) &&
+                        Objects.equals(expectedType, actualCmd.type())
+        ));
+
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallsCreateCastMember_shouldReturnNotification() throws Exception {
+        //given
+        final String expectedName = null;
+        final var expectedType = Fixture.CastMember.type();
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = new CreateCastMemberRequest(expectedName, expectedType);
+
+        when(createCastMemberUseCase.execute(any())).thenThrow(NotificationException.with(new Error(expectedErrorMessage)));
+
+        //when
+        final var aRequest = post("/cast_members").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(aCommand));
+
+
+        final var response = this.mvc.perform(aRequest).andDo(print());
+
+        //then
+
+        response.andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
+
+        verify(createCastMemberUseCase).execute(argThat(actualCmd ->
+                Objects.equals(expectedName, actualCmd.name()) &&
+                        Objects.equals(expectedType, actualCmd.type())
+        ));
+    }
 }
