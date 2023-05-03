2dfcdd47ced4da8b5a1af6be069bc5c98ebcebcc
Vicente Junior <vicente19981@live.com>
2023-05-03 20:23:36 -0300

    feat: adding exists by id

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/genre/GenreGateway.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/genre/GenreGateway.java
@@ -3,6 +3,7 @@ package com.fullcycle.admin.catalog.domain.genre;
 import com.fullcycle.admin.catalog.domain.pagination.Pagination;
 import com.fullcycle.admin.catalog.domain.pagination.SearchQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GenreGateway {
@@ -15,4 +16,6 @@ public interface GenreGateway {
     Genre update(Genre genre);
 
     Pagination<Genre> findAll(SearchQuery aQuery);
+
+    List<GenreID> existsById(Iterable<GenreID> ids);
 }
