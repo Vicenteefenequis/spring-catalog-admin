ec1f0c09efd84fc5791e5bd47833a188d3703748
Vicente Junior <vicente19981@live.com>
2023-05-03 20:24:21 -0300

    feat: create video use case

---
+++ application/src/main/java/com/fullcycle/admin/catalog/application/video/create/CreateVideoUseCase.java
@@ -0,0 +1,7 @@
+package com.fullcycle.admin.catalog.application.video.create;
+
+import com.fullcycle.admin.catalog.application.UseCase;
+
+public abstract class CreateVideoUseCase
+        extends UseCase<CreateVideoCommand, CreateVideoOutput> {
+}
