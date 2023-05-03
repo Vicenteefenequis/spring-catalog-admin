619865328190b561c0ffcac709604b94e175453e
Vicente Junior <vicente19981@live.com>
2023-05-03 20:37:36 -0300

    feat: create media resource gateway

---
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/video/MediaResourceGateway.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalog.domain.video;
+
+public interface MediaResourceGateway {
+    AudioVideoMedia storeAudioVideo(VideoID anId, Resource aResource);
+
+    ImageMedia storeImage(VideoID anId, Resource aResource);
+
+    void clearResources(VideoID anId);
+}
