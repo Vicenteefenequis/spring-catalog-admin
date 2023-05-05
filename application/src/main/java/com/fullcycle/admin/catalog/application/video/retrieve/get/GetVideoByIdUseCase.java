7c8bcc4062935c26d20962fdbc033b7a69283711
Vicente Junior <vicente19981@live.com>
2023-05-05 13:44:02 -0300

    feat: create abstract get video by id usecase

---
+++ application/src/main/java/com/fullcycle/admin/catalog/application/video/retrieve/get/GetVideoByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admin.catalog.application.video.retrieve.get;
+
+import com.fullcycle.admin.catalog.application.UseCase;
+
+public abstract class GetVideoByIdUseCase extends UseCase<String, VideoOutput> {
+}
