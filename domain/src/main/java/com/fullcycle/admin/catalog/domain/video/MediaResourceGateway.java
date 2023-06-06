aacb12a37b6d916e0b871ab2a4987bf431430c80
Vicente Junior <vicente19981@live.com>
2023-06-06 19:26:38 -0300

    feat: create optional get resource

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/video/MediaResourceGateway.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/video/MediaResourceGateway.java
@@ -1,9 +1,15 @@
 package com.fullcycle.admin.catalog.domain.video;
 
+import com.fullcycle.admin.catalog.domain.resource.Resource;
+
+import java.util.Optional;
+
 public interface MediaResourceGateway {
     AudioVideoMedia storeAudioVideo(VideoID anId, VideoResource aResource);
 
     ImageMedia storeImage(VideoID anId, VideoResource aResource);
+
+    Optional<Resource> getResource(VideoID anId, VideoMediaType aType);
 
     void clearResources(VideoID anId);
 }
