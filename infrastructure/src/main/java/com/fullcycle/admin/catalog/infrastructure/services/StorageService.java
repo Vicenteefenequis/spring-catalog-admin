cf7c7926f6342b64820e80b898db3e6bbc3e56a1
Vicente Junior <vicente19981@live.com>
2023-06-06 08:17:13 -0300

    feat: create GCStorageService implement

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/services/StorageService.java
@@ -0,0 +1,19 @@
+package com.fullcycle.admin.catalog.infrastructure.services;
+
+import com.fullcycle.admin.catalog.domain.video.Resource;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface StorageService {
+
+    void deleteAll(Collection<String> names);
+
+    Optional<Resource> get(String name);
+
+    List<String> list(String prefix);
+
+    void store(String name, Resource resource);
+
+}
