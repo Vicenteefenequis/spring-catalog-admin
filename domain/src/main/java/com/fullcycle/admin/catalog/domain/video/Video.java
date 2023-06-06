0aa677c3eb1f201f4a297d88aa732915152ec963
Vicente Junior <vicente19981@live.com>
2023-06-06 20:56:25 -0300

    refactor: create a news methods

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/video/Video.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/video/Video.java
@@ -82,7 +82,6 @@ public class Video extends AggregateRoot<VideoID> {
     }
 
 
-
     @Override
     public void validate(ValidationHandler handler) {
         new VideoValidator(this, handler).validate();
@@ -339,5 +338,23 @@ public class Video extends AggregateRoot<VideoID> {
                 genres,
                 members
         );
+    }
+
+    public Video processing(final VideoMediaType aType) {
+        if (VideoMediaType.VIDEO == aType) {
+            getVideo().ifPresent(media -> setVideo(media.processing()));
+        } else if (VideoMediaType.TRAILER == aType) {
+            getTrailer().ifPresent(media -> setTrailer(media.processing()));
+        }
+        return this;
+    }
+
+    public Video completed(final VideoMediaType aType, final String encodedPath) {
+        if (VideoMediaType.VIDEO == aType) {
+            getVideo().ifPresent(media -> setVideo(media.completed(encodedPath)));
+        } else if (VideoMediaType.TRAILER == aType) {
+            getTrailer().ifPresent(media -> setTrailer(media.completed(encodedPath)));
+        }
+        return this;
     }
 }
