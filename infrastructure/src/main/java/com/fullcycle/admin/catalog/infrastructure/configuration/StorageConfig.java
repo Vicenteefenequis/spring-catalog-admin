218c51b7f176bb38b66d9c3b0d0ff784b728ca01
Vicente Junior <vicente19981@live.com>
2023-06-06 14:30:26 -0300

    feat: config bean of storage properties

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/StorageConfig.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/StorageConfig.java
@@ -1,18 +1,27 @@
 package com.fullcycle.admin.catalog.infrastructure.configuration;
 
 
-import com.fullcycle.admin.catalog.infrastructure.configuration.properties.GoogleStorageProperties;
+import com.fullcycle.admin.catalog.infrastructure.configuration.properties.google.GoogleStorageProperties;
+import com.fullcycle.admin.catalog.infrastructure.configuration.properties.storage.StorageProperties;
 import com.fullcycle.admin.catalog.infrastructure.services.StorageService;
 import com.fullcycle.admin.catalog.infrastructure.services.impl.GCStorageService;
 import com.fullcycle.admin.catalog.infrastructure.services.local.InMemoryStorageService;
 import com.google.cloud.storage.Storage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class StorageConfig {
+
+    @Bean
+    @ConfigurationProperties(value = "storage.catalog-videos")
+    public StorageProperties storageProperties() {
+        return new StorageProperties();
+    }
+
     @Bean(name = "storageService")
     @Profile({"development", "production"})
     public StorageService gcStorageService(
