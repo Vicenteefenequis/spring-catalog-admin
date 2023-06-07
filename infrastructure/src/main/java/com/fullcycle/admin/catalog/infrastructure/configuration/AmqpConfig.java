c5cbc0430dfc57e888f4e68ecf4e43c34532ec10
Vicente Junior <vicente19981@live.com>
2023-06-06 23:23:36 -0300

    feat: create amqp config

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/AmqpConfig.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/AmqpConfig.java
@@ -3,7 +3,9 @@ package com.fullcycle.admin.catalog.infrastructure.configuration;
 
 import com.fullcycle.admin.catalog.infrastructure.configuration.annotations.VideoCreatedQueue;
 import com.fullcycle.admin.catalog.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.fullcycle.admin.catalog.infrastructure.configuration.annotations.VideoEvents;
 import com.fullcycle.admin.catalog.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,22 +14,60 @@ import org.springframework.context.annotation.Configuration;
 public class AmqpConfig {
 
     @Bean
-    @ConfigurationProperties("amqp.queue.video-created")
+    @ConfigurationProperties("amqp.queues.video-created")
     @VideoCreatedQueue
     public QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
-    @ConfigurationProperties("amqp.queue.video-encoded")
+    @ConfigurationProperties("amqp.queues.video-encoded")
     @VideoEncodedQueue
     public QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
     }
 
-    @Bean
-    public String queueName(@VideoCreatedQueue QueueProperties queueProperties) {
-        return queueProperties.getQueue();
+
+    @Configuration
+    static class Admin {
+        @Bean
+        @VideoEvents
+        public Exchange videoEventsExchange(@VideoCreatedQueue QueueProperties props) {
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Queue videoCreatedQueue(@VideoCreatedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Binding videoCreatedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoCreatedQueue Queue queue,
+                @VideoCreatedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Queue videoEncodedQueue(@VideoEncodedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+
+        @Bean
+        @VideoEncodedQueue
+        public Binding videoEncodedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoEncodedQueue Queue queue,
+                @VideoEncodedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
     }
 
 }
