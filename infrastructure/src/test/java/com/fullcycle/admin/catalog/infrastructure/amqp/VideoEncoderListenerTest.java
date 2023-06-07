824e4e1eb969967ba8fb1ede8457259089a5679d
Vicente Junior <vicente19981@live.com>
2023-06-07 20:48:59 -0300

    test: ensure VideoEncoderListener given a completed result and error result

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/amqp/VideoEncoderListenerTest.java
@@ -0,0 +1,111 @@
+package com.fullcycle.admin.catalog.infrastructure.amqp;
+
+import com.fullcycle.admin.catalog.AmqpTest;
+import com.fullcycle.admin.catalog.application.video.media.update.UpdateMediaStatusCommand;
+import com.fullcycle.admin.catalog.application.video.media.update.UpdateMediaStatusUseCase;
+import com.fullcycle.admin.catalog.domain.utils.IdUtils;
+import com.fullcycle.admin.catalog.domain.video.MediaStatus;
+import com.fullcycle.admin.catalog.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.fullcycle.admin.catalog.infrastructure.configuration.json.Json;
+import com.fullcycle.admin.catalog.infrastructure.configuration.properties.amqp.QueueProperties;
+import com.fullcycle.admin.catalog.infrastructure.video.models.VideoEncoderCompleted;
+import com.fullcycle.admin.catalog.infrastructure.video.models.VideoEncoderError;
+import com.fullcycle.admin.catalog.infrastructure.video.models.VideoMessage;
+import com.fullcycle.admin.catalog.infrastructure.video.models.VideoMetadata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.amqp.rabbit.test.RabbitListenerTestHarness;
+import org.springframework.amqp.rabbit.test.TestRabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@AmqpTest
+public class VideoEncoderListenerTest {
+
+    @Autowired
+    private TestRabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private RabbitListenerTestHarness harness;
+
+    @MockBean
+    private UpdateMediaStatusUseCase updateMediaStatusUseCase;
+
+    @Autowired
+    @VideoEncodedQueue
+    private QueueProperties queueProperties;
+
+    @Test
+    public void givenErrorResult_whenCallsListener_shouldProcess() throws InterruptedException {
+        //given
+        final var expectedError = new VideoEncoderError(
+                new VideoMessage("123", "abc"),
+                "Video Not Found"
+        );
+
+        final var expectedMessage = Json.writeValueAsString(expectedError);
+        //when
+
+        this.rabbitTemplate.convertAndSend(queueProperties.getQueue(), expectedMessage);
+        //then
+
+        final var invocationData = harness.getNextInvocationDataFor(VideoEncoderListener.LISTENER_ID, 1, TimeUnit.SECONDS);
+
+        Assertions.assertNotNull(invocationData);
+        Assertions.assertNotNull(invocationData.getArguments());
+
+        final var actualMessage = (String) invocationData.getArguments()[0];
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+    }
+
+
+    @Test
+    public void givenCompletedResult_whenCallsListener_shouldCallsUseCase() throws InterruptedException {
+        //given
+        final var expectedId = IdUtils.uuid();
+        final var expectedOutputBucket = "codeeducationtest";
+        final var expectedStatus = MediaStatus.COMPLETED;
+        final var expectedEncoderVideoFolder = "any-folder";
+        final var expectedResourceId = IdUtils.uuid();
+        final var expectedFilePath = "any-file-path";
+        final var expectedMetadata = new VideoMetadata(expectedEncoderVideoFolder, expectedResourceId, expectedFilePath);
+
+        final var aResult = new VideoEncoderCompleted(
+                expectedId,
+                expectedOutputBucket,
+                expectedMetadata
+        );
+
+        final var expectedMessage = Json.writeValueAsString(aResult);
+        doNothing().when(updateMediaStatusUseCase).execute(any());
+
+        //when
+        this.rabbitTemplate.convertAndSend(queueProperties.getQueue(), expectedMessage);
+        //then
+        final var invocationData = harness.getNextInvocationDataFor(VideoEncoderListener.LISTENER_ID, 1, TimeUnit.SECONDS);
+
+        Assertions.assertNotNull(invocationData);
+        Assertions.assertNotNull(invocationData.getArguments());
+
+        final var actualMessage = (String) invocationData.getArguments()[0];
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+        final var cmdCaptor = ArgumentCaptor.forClass(UpdateMediaStatusCommand.class);
+        verify(updateMediaStatusUseCase).execute(cmdCaptor.capture());
+
+        final var actualCommand = cmdCaptor.getValue();
+        Assertions.assertEquals(expectedStatus, actualCommand.status());
+        Assertions.assertEquals(expectedId, actualCommand.videoId());
+        Assertions.assertEquals(expectedResourceId, actualCommand.resourceId());
+        Assertions.assertEquals(expectedEncoderVideoFolder, actualCommand.folder());
+        Assertions.assertEquals(expectedFilePath, actualCommand.filename());
+    }
+
+}
