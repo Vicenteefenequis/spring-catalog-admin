c97eebfee076a92ef65bb79ad7ca5700996a7b7d
Vicente Junior <vicente19981@live.com>
2023-06-07 18:39:26 -0300

    feat: create annotation of amqp test

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/AmqpTest.java
@@ -0,0 +1,15 @@
+package com.fullcycle.admin.catalog;
+
+import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@SpringBootTest(classes = WebServerConfig.class)
+public @interface AmqpTest {
+}
