de814e49bbe7697318aa2850ed729cee36442792
Vicente Junior <vicente19981@live.com>
2023-06-11 14:08:59 -0300

    feat: create update video response

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/video/models/UpdateVideoResponse.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalog.infrastructure.video.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record UpdateVideoResponse(
+        @JsonProperty("id") String id
+) {
+}
