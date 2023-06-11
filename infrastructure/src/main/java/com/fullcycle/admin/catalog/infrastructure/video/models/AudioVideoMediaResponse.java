321086d74cf5f753dc78ee636df769c1930a79f4
Vicente Junior <vicente19981@live.com>
2023-06-11 13:46:23 -0300

    feat: create audio video media response

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/video/models/AudioVideoMediaResponse.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/video/models/AudioVideoMediaResponse.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record AudioVideoMediaResponse(
         @JsonProperty("id") String id,
         @JsonProperty("checksum") String checksum,
-        @JsonProperty("checksum") String name,
+        @JsonProperty("name") String name,
         @JsonProperty("location") String rawLocation,
         @JsonProperty("encoded_location") String encodedLocation,
-        @JsonProperty("mime_type") String status
+        @JsonProperty("status") String status
 ) {
 }
