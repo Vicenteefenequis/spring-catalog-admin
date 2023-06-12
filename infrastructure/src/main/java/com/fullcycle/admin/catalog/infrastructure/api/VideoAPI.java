7081abca0ac81a9cd0d4428f87675323d69473a7
Vicente Junior <vicente19981@live.com>
2023-06-12 15:30:53 -0300

    test: adding more asserts

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/api/VideoAPI.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/api/VideoAPI.java
@@ -114,4 +114,17 @@ public interface VideoAPI {
             @ApiResponse(responseCode = "500", description = "An internal error occurred")
     })
     void deleteById(@PathVariable(name = "id") String id);
+
+
+    @GetMapping(value = "{id}/medias/{type}")
+    @Operation(summary = "Get a video media by it`s type")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Media retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Media was not found"),
+            @ApiResponse(responseCode = "500", description = "An internal error occurred")
+    })
+    ResponseEntity<byte[]> getMediaByType(
+            @PathVariable(name = "id") String id,
+            @PathVariable(name = "type") String type
+    );
 }
