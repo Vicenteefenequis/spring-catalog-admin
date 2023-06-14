6118bb24d183feb66fefee6c4a8467ac6274d100
Vicente Junior <vicente19981@live.com>
2023-06-14 20:47:33 -0300

    feat: adding environment to a sandbox

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,9 +1,11 @@
 
+docker network create admin_video_services
+
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
 
 
-docker compose -f app/docker-compose.yml up -d
+docker compose -f services/docker-compose.yml up -d
 
 echo "Inicializando containers..."
 sleep 20
