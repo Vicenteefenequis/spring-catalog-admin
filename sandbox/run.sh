bef69f0554104c10ae84078d67fd2b0529889841
Vicente Junior <vicente19981@live.com>
2023-06-14 21:06:59 -0300

    feat: adding kibana

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,11 +1,15 @@
 
 docker network create admin_video_services
+docker network create elastic
 
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
 
 
+#docker compose -f app/docker-compose.yml up -d
 docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
 
 echo "Inicializando containers..."
 sleep 20
